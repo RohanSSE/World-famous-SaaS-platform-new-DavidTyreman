a7631c7aeafab56711abc88c199b646ea66662a8
RohanSSE <rohana@synapseco.com>
2026-05-21 12:26:57 +0530

    Admin-v0.1

---
+++ admin/src/config-global.ts
@@ -0,0 +1,10 @@
+import packageJson from '../package.json';
+// ----------------------------------------------------------------------
+export type ConfigValue = {
+  appName: string;
+  appVersion: string;
+};
+export const CONFIG: ConfigValue = {
+  appName: 'Minimal UI',
+  appVersion: packageJson.version,
+};
